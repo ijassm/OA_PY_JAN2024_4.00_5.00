4145a70da199616b4b19e93815f58148e19a1407
“ijassm” <ijass.ocean@gmail.com>
2024-01-29 17:20:30 +0530

    -- 29 JAN 2024

---
+++ nested_forloop.py
@@ -0,0 +1,63 @@
+# for i in range(1, 3):
+#     for j in range(1, 3):
+#         for k in range(1, 3):
+#             print(i, j, k)
+
+# i = 1
+##### j = 1 | j = 2
+# i = 2
+##### j = 1 | j = 2
+
+# i = 1
+### j = 1
+# k = 1 | k = 2
+### j = 2
+# k = 1 | k = 2
+# i = 2
+### j = 1
+# k = 1 | k = 2
+### j = 2
+# k = 1 | k = 2
+
+
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         print("*", end=" ")
+#     print()
+
+
+# The pattern like :
+
+# *
+# **
+# ***
+# ****
+# n = 10
+
+# for i in range(1, n + 1):
+#     for j in range(1, i + 1):
+#         print("*", end=" ")
+#     print()
+
+
+# 1 2 3 4 5
+# 1 2 3 4 5
+# 1 2 3 4 5
+# 1 2 3 4 5
+
+n = 5
+
+# for i in range(1, n):
+#     for j in range(1, 6):
+#         print(j, end=" ")
+#     print()
+
+# 5 4 3 2 1
+# 5 4 3 2 1
+# 5 4 3 2 1
+# 5 4 3 2 1
+
+for i in range(1, n):
+    for j in range(5, 0, -1):
+        print(j, end=" ")
+    print()
