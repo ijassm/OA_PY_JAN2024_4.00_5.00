1cb266c06b553184c1a9b351ccd0465b4b3a4c24
“ijassm” <ijass.ocean@gmail.com>
2024-01-29 17:27:35 +0530

    -- 29 JAN 2024

--- nested_forloop.py
+++ nested_forloop.py
@@ -57,7 +57,7 @@ n = 5
 # 5 4 3 2 1
 # 5 4 3 2 1
 
-for i in range(1, n):
-    for j in range(5, 0, -1):
-        print(j, end=" ")
-    print()
+# for i in range(1, n):
+#     for j in range(5, 0, -1):
+#         print(j, end=" ")
+#     print()
