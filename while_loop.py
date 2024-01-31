34995febd1be78da08b1fb094dc40350023b7b18
“ijassm” <ijass.ocean@gmail.com>
2024-01-31 17:06:25 +0530

    -- 31 JAN 2024

--- while_loop.py
+++ while_loop.py
@@ -1,10 +1,83 @@
 # while condition:
 #     pass
 
-a = 142
+# a = 142
 
-i = 0
+# i = 0
 
-while i < 10:
-    i += 1
-    print(i)
+# while i < 10:
+#     i += 1
+#     print(i)
+
+
+# 1. Write a C program to print all natural numbers from 1 to n. – using while loop
+
+# n = int(input("Enter the n number: "))
+# i = 0
+
+# while i < n:
+#     i += 1
+#     print(i)
+
+# 2.Write a C program to print all natural numbers in reverse (from n to 1). – using while loop
+
+# n = int(input("Enter the n number: "))
+# i = 1
+
+# while n >= i:
+#     print(n)
+#     n -= 1
+
+# 3.Write a C program to print all alphabets from a to z. – using while loop
+
+# start = 97
+# stop = 122
+
+# # print(chr(97))
+
+# while start != stop + 1:
+#     print(chr(start))
+#     start += 1
+
+# 4.Write a C program to print all even numbers between 1 to 100. – using while loop
+
+# i = 0
+# stop = 100
+
+# while i < stop:
+#     i += 2
+#     print(i)
+
+# 5.Write a C program to print all odd number between 1 to 100.
+
+# i = 1
+# stop = 100
+
+# while i < stop:
+#     print(i)
+#     i += 2
+
+# 6. Write a C program to find sum of all natural numbers between 1 to n.
+
+# i = 0
+# sum = 0
+# n = 10
+
+# while i < n:
+#     i += 1
+#     sum += i
+
+# print("sum =", sum)
+
+# 7. Write a C program to find sum of all even numbers between 1 to n.
+
+# i = 0
+# evenSum = 0
+# n = 10
+
+# while i < n:
+#     i += 2
+#     evenSum += i
+#     print("even num =", i)
+
+# print("Total =", evenSum)
