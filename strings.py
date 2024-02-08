421b9a7b7a053a8443e4a716002410036a6e35ad
“ijassm” <ijass.ocean@gmail.com>
2024-02-08 17:53:32 +0530

    -- 08 FEB 2024

--- strings.py
+++ strings.py
@@ -35,7 +35,7 @@
 # print(output)
 
 
-# Given a string S. The task is to convert characters of string to lowercase.
+# 3. Given a string S. The task is to convert characters of string to lowercase.
 
 # s = "ABCDdE"
 
@@ -55,7 +55,7 @@
 # print(s, "before conversion")
 # print(output, "after conversion")
 
-# Given a String S , print the reverse of the string as output.
+# 4. Given a String S , print the reverse of the string as output.
 
 # s = "ymedacanaeco"
 # s = "malayalam"
@@ -68,7 +68,7 @@
 
 # print(output)
 
-# Given a string str, convert the first letter of each word in the string to uppercase.
+# 5. Given a string str, convert the first letter of each word in the string to uppercase.
 
 # str = "i love programming"
 # l = str.split(" ")
@@ -83,7 +83,7 @@
 # print(output)
 
 
-# Given a string consisting of lowercase english alphabets, reverse only the vowels present in it and print the resulting string.
+# 6. Given a string consisting of lowercase english alphabets, reverse only the vowels present in it and print the resulting string.
 
 # s = "practice"
 # vowels = []
@@ -93,7 +93,7 @@
 #         print(i)
 
 
-# Given a string, check if all its characters are the same or not.
+# 7. Given a string, check if all its characters are the same or not.
 
 # s = "aaaaab"
 
@@ -110,7 +110,7 @@
 # print(output)
 
 
-# Given a string S as input. Delete the characters at odd indices of the string.
+# 8. Given a string S as input. Delete the characters at odd indices of the string.
 
 # s = "ocean"
 # length = len(s)
@@ -123,7 +123,7 @@
 # print(output)
 
 """
-Given a string S which consists of alphabets , 
+9. Given a string S which consists of alphabets , 
 numbers and special characters. 
 You need to write a program to split the strings in three different 
 strings S1, S2 and S3 such that the string S1 will contain all the 
@@ -161,7 +161,7 @@ The strings S1, S2 and S3 should have characters in same order as they appear in
 # print(s3)
 
 
-# Given a string s, extract all the integers from s.
+# 10. Given a string s, extract all the integers from s.
 
 # s = "1: Prakhar Agrawal, 2: Manish Kumar Rai, 3: Rishabh Gupta56"
 # output = ""
@@ -171,3 +171,32 @@ The strings S1, S2 and S3 should have characters in same order as they appear in
 #         output += " " + i
 
 # print(output)
+
+"""
+11. Given a string of a constant length, print a triangle out of it. 
+The triangle should start with the given string and keeps shrinking downwards 
+by removing one character from the begining of the string. The spaces on the 
+left side of the triangle should be replaced with dot character.
+"""
+
+# s = "oceanacademy"
+# length = len(s)
+# output = ""
+
+# for i in range(length):
+#     output = "." * i + s[i:]
+#     print(output)
+
+
+"""
+12. Given a string S, consisting only of english alphabets, 
+replace all the alphabets with the alphabets occuring at the same 
+position when counted in reverse order of alphabets. 
+For example, 'a' would be replaced by 'z', 'b' by 'y', 'c' by 'x' 
+and so on. Any capital letters would be replaced by capital letters only.
+"""
+
+# s = "hello"
+
+# if(True):
+#     pass
