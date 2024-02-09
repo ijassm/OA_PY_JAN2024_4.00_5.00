e55d1c5529f17fd15350a25a5fe2ed6987c3137b
“ijassm” <ijass.ocean@gmail.com>
2024-02-09 17:35:16 +0530

    list intro & list methods-- 09 FEB 2024

---
+++ list.py
@@ -0,0 +1,72 @@
+# even1 = 2
+# even2 = 4
+
+# [] = > list
+
+even = [2, 4, 6, 8, 10, 12, 12, 12]
+
+# accessing index
+# print("positive index values")
+# print(even[0], "even[0]")
+# print(even[1], "even[1]")
+# print(even[2], "even[2]")
+# print(even[3], "even[3]")
+# print(even[4], "even[4]\n")
+
+# print("negative index values")
+# print(even[-1], "even[-1]")
+# print(even[-2], "even[-2]")
+# print(even[-3], "even[-3]")
+# print(even[-4], "even[-4]")
+# print(even[-5], "even[-5]")
+
+# mutable
+# print(even, "before")
+
+# even[0] = 14
+
+# print(even, "after")
+# print(type(even))
+
+# list methods
+
+# print(even, "before")
+
+# append - Adds an element at the end of the list
+# even.append(16)
+
+# clear - Removes all the elements from the list
+
+# even.clear()
+
+# copy - Returns a copy of the list
+
+# a = even
+# b = even.copy()
+# c = even
+
+# c[0] = 18
+
+# print(a, "a")
+# print(b, "b")
+# print(c, "c")
+# print(even, "even")
+
+# print(id(a))
+# print(id(b))
+# print(id(c))
+# print(id(even))
+
+
+# count()	Returns the number of elements with the specified value
+
+# print(even.count(12))
+
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+
+# a.extend(b)
+
+# print(a)
+
+# https://www.w3schools.com/python/python_ref_list.asp
