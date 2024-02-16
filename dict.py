dd0e72968a9b9f96e009b1ef2adba2141fe1237a
“ijassm” <ijass.ocean@gmail.com>
2024-02-16 17:27:02 +0530

    worked out dict problems -- 16 FEB 2024

--- dict.py
+++ dict.py
@@ -2,19 +2,48 @@
 # ordered
 # it will be in key : value order
 
-person = {
-    "name": "John",
-    "age": 30,
-    "city": "New York",
-    "skills": ["PY", "JAVA", "JS", "DART"],
-}
+# person = {
+#     "name": "John",
+#     "age": 30,
+#     "city": "New York",
+#     "skills": ["PY", "JAVA", "JS", "DART"],
+#     "address": "New York, NY, USA",
+# }
 
 # personList = ["John", 30, "New York"]
 
 
-print(person)
-print(person["name"])
-print(person["age"])
-print(person["city"])
-print(person["skills"])
-print(type(person))
+# print(person)
+# print(person["name"])
+# print(person["age"])
+# print(person["city"])
+# print(person["skills"])
+# print(type(person))
+
+
+# person["address"] = "Pondy"
+# person.update({"address": "Pondy"})
+
+# print(person)
+
+
+# 1. Write a Python script to sort (ascending and descending) a dictionary by value.
+
+d = {"e": 5, "c": 3, "a": 1, "d": 4, "b": 2}
+items = []
+
+# print(d.keys())
+# print(d.values())
+
+for i in d:
+    items.append((i, d[i]))
+
+print(items, "original value")
+length = len(items)
+
+for i in range(length):
+    for j in range(i + 1, length):
+        if items[i][1] > items[j][1]:
+            items[i], items[j] = items[j], items[i]
+
+print(items, "ascending value")
