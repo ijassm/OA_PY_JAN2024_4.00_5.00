2d65f8e55db1c19352531e75299afeb01370f47f
“ijassm” <ijass.ocean@gmail.com>
2024-03-04 17:54:54 +0530

    REFACTOR : recurrsion

--- recursive-func.py
+++ recursive-func.py
@@ -1,7 +1,24 @@
-def recursiveFunc(a):
-    if a > 0:
-        recursiveFunc(a - 1)
-    print("Recursive function", a)
+def recursiveFactorial(a):
+    if a == 0:
+        return 1
+    else:
+        return a * recursiveFactorial(a - 1)
 
 
-recursiveFunc(10)
+print(recursiveFactorial(5))
+
+
+# def factorial(a):
+#     f = 1
+#     for i in range(1, a + 1):
+#         f *= i
+#     return f
+
+
+# print(factorial(5))
+
+# Testcases
+# sum(2)() = > 2
+# sum(2)(4)() = > 6
+# sum(2)(4)(4)() = > 10
+# sum(2)(4)(4)(2)() = > 12
